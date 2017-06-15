7f21276b3c0afa640a5d4a40d936821a50e7f2b0
Jordan Lee <jordanlee151@gmail.com>
2017-06-15 09:10:34 +0000

    Allowed the contact form to save to database

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,5 +1,19 @@
 class ContactsController < ApplicationController
     def new
-
+        @contact = Contact.new
     end
+    
+    def create
+        @contact = Contact.new(contact_params)
+        if @contact.save
+            redirect_to new_contact_path, notice: "Successfully sent!"
+        else
+            redirect_to new_contact_path, notice: "Could not send."
+        end
+    end
+    
+    private
+        def contact_params
+            params.require(:contact).permit(:name, :email, :comments)
+        end
 end
