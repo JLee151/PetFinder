d8e5dad57521a43a673d8bdd73965d184ffb382c
Jordan Lee <jordanlee151@gmail.com>
2017-07-25 08:34:34 +0000

    Added home page plan links to handle query string parameters

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,12 @@
 class PagesController < ApplicationController
+
+	# GET request for / -> home page
 	def home
-	
+		@base_plan = Plan.find(1)
+		@pro_plan = Plan.find(2)
 	end
-	
+
 	def about
-	
+
 	end
 end
